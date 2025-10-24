54d0b78f80568be6b9cb8fbaf6e4a25195677d7c
AnhDungPham2901 <dungpham290198@gmail.com>
2025-10-24 14:58:43 +0700

    drill 2.4 uses error function from PPP_support

--- Chapter2/Drills/dr_2.4.cpp
+++ Chapter2/Drills/dr_2.4.cpp
@@ -9,10 +9,20 @@ int main() {
     cout << "Enter the first name of another friend that you want to metion" << '\n';
     cin >> friend_name;
 
-    cout << "Dear " << first_name << ",\n"
-            << " How are you? I'm fine. I miss you "
-            << "I have just strated a new job as an AI Engineer. "
-            << "How is your job? "
-            << "Have you seen " << friend_name << " lately? \n";
+    int age;
+    cout << "Enter the age of the receipient \n";
+    cin >> age;
+
+    if((age < 0) || (age > 110)) {
+        error("You're kidding! \n");
+    }
+    else {
+        cout << "Dear " << first_name << ",\n"
+                << " How are you? I'm fine. I miss you "
+                << "I have just strated a new job as an AI Engineer. "
+                << "How is your job? "
+                << "Have you seen " << friend_name << " lately? "
+                << "I hear you just had a birthday and you are " << age << " years old\n";
+    }
     return 0;
 }
