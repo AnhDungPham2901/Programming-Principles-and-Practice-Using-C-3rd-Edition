a92d86f90e4884b536aec8c96ea1c4e5e37a9331
AnhDungPham2901 <dungpham290198@gmail.com>
2025-10-27 18:53:30 +0700

    first drill of chap 3

---
+++ Chapter3/TryThis/tt_3.1.cpp
@@ -0,0 +1,23 @@
+#include "../../PPPHeaders.h"
+
+int main() {
+    char currency = ' ';
+    double amount = 1;
+    constexpr double usd_to_yen = 152.74;
+    constexpr double usd_to_kroner = 10.00;
+    constexpr double usd_to_pounds = 0.75;
+
+
+    cout<< "Please enter an amount followed by its currency (y or k or p):\n";
+    cin >> amount >> currency;
+
+    if (currency == 'y')
+        cout << amount << "yen == " << amount/usd_to_yen << "dollars\n";
+    else if (currency == 'k')
+        cout << amount << "kroner == " << amount/usd_to_kroner << "dollars\n";
+    else if (currency == 'p')
+        cout << amount << "pounds == " << amount/usd_to_pounds << "dollars\n";
+    else
+         cout << "Sorry, I don't know a currency called '" << currency << "'\n";
+    return 0;
+}
