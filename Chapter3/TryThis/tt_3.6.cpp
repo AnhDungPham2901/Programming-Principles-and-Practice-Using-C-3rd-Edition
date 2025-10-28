704f4d129a475354eb7ba8e54ad38bc1dabd3824
AnhDungPham2901 <dungpham290198@gmail.com>
2025-10-28 18:43:57 +0700

    more words with try this 2.6

--- Chapter3/TryThis/tt_3.6.cpp
+++ Chapter3/TryThis/tt_3.6.cpp
@@ -1,19 +1,27 @@
 #include "../../PPPHeaders.h"
 
-int main() {
-    string disliked = "Broccoli";
+bool is_not_in_disliked_words(string word, vector<string> disliked_words) {
+    for(int i=0; i<disliked_words.size(); ++i) {
+        if(word == disliked_words[i])
+            return false;
+    }
+    return true;
+}
 
+int main() {
+    vector<string> disliked_words = {"Broccoli", "Haha" ,"Test"};
     cout << "Please enter words you want to print out \n";
 
     vector<string> words;
     for(string temp; cin >> temp;) {
         words.push_back(temp);
     }
+
+    bool is_not_in_disliked_words(string word, vector<string> diskliked_words);
     
     for(int i=0; i<words.size(); ++i) {
-        if(words[i] != disliked) {
+        if(is_not_in_disliked_words(words[i], disliked_words))
             cout << words[i] << ' ';
-        }
     }
     cout << '\n';
     return 0;
