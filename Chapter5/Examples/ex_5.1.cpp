9532e060bea94d736312290a7c765493d4542196
Anh Dung Pham <dungpham290198@gmail.com>
2025-12-20 08:15:28 +0700

    first commit for chap5

---
+++ Chapter5/Examples/ex_5.1.cpp
@@ -0,0 +1,16 @@
+#include "../../PPPHeaders.h"
+
+
+class Token {
+    public:
+        char kind;
+        double value;
+        Token(char k) : kind{k}, value{0.0}{} // construction from one value
+        Token(char k, double v) : kind{k}, value{v} {} // construction from two values
+
+}; 
+
+int main() {
+    Token t {'+'}; 
+    return 0;
+}
